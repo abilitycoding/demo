e361099e7deab052b1b0c821db733c7fd55aceb1
abilitycoding <abilitycoding@gmail.com>
2024-06-04 20:53:16 +0530

    new commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/login", (req, res) => {
+  res.send({ name: "sandy" });
+});
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
